e48eab03e147e6842523d9ab201990c2255f31bb
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-07-01 15:46:28 +0530

    del pervious all

--- reduxapp/src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import AlternateNumber from "./idfunc";
-
-import { combineReducers } from "redux";
-
-const Rootreducer=combineReducers({
-    AlternateNumber
-});
-
-export default Rootreducer;
--- todoredux/src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import todoreducer from "./reducer";
-
-import { combineReducers } from "redux";
-
-const rootReducers=combineReducers({todoreducer})
-
-export default rootReducers
-
